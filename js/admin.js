c439a7b2f96fae6342ddffcd79735dcad19677d4
LourdesGarcia <lourdesgarciafyl@gmail.com>
2023-05-12 21:04:44 -0300

    se traen al admin js los campos del formulario, el btn crearJuego y el modal con el formulario

--- js/admin.js
+++ js/admin.js
@@ -2,3 +2,24 @@ import Juego from "./classJuego.js"
 import { sumarioValidacion } from "./helpers.js"
 
 let formularioAdminJuego = document.getElementById(`formJuego`);
+
+let btnCrearJuego = document.getElementById(`btnCrearJuego`);
+let modalJuegos = new bootstrap.Modal(document.getElementById(`modalJuegos`));
+let codigo = document.getElementById(`codigo`);
+let nombre = document.getElementById(`nombre`);
+let precio = document.getElementById(`precio`);
+let precioOferta = document.getElementById(`precioOferta`);
+let descripcion = document.getElementById(`descripcion`); 
+let imagen = document.getElementById(`imagen`); 
+let trailer = document.getElementById(`trailer`); 
+let categoria = document.getElementById(`categoria`); 
+let desarrollador = document.getElementById(`desarrollador`); 
+let almacenamiento = document.getElementById(`almacenamiento`); 
+let placaGrafica = document.getElementById(`placa`); 
+let ram = document.getElementById(`ram`); 
+let procesador = document.getElementById(`procesador`);
+
+btnCrearJuego.addEventListener(`click`, mostrarFormularioJuego)
+function mostrarFormularioJuego(){
+    modalJuegos.show()
+}
--- js/classJuego.js
+++ js/classJuego.js
@@ -66,7 +66,7 @@ export default class Juego {
     }
   
     set descripcion(descripcion) {
-      this.#descripcio = descripcion;
+      this.#descripcion = descripcion;
     }
   
     get imagen() {
