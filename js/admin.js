cc92242efd3cdc430cf427ca2dc026dc647a8c8b
LourdesGarcia <lourdesgarciafyl@gmail.com>
2023-05-13 13:43:24 -0300

    se agregan funciones relacionadas a poner o no juegos en oferta y se cambia el div de juegos en oferta para que solo aparezca cuando el admin ponga SI

--- js/admin.js
+++ js/admin.js
@@ -1,6 +1,9 @@
 import Juego from "./classJuego.js"
 import { sumarioValidacion } from "./helpers.js"
-
+// listas de juegos y juegos en oferta
+let listaJuegos = [];
+let listaJuegosOferta = [];
+// traigo el modal, el formulario y el boton de crear juego
 let formularioAdminJuego = document.getElementById(`formJuego`);
 let btnCrearJuego = document.getElementById(`btnCrearJuego`);
 let modalJuegos = new bootstrap.Modal(document.getElementById(`modalJuegos`));
@@ -8,7 +11,7 @@ let modalJuegos = new bootstrap.Modal(document.getElementById(`modalJuegos`));
 let codigo = document.getElementById(`codigo`);
 let nombre = document.getElementById(`nombre`);
 let precio = document.getElementById(`precio`);
-let precioOferta = document.getElementById(`precioOferta`);
+let precioParaJuegoEnOferta = null;
 let descripcion = document.getElementById(`descripcion`); 
 let imagen = document.getElementById(`imagen`); 
 let trailer = document.getElementById(`trailer`); 
@@ -18,13 +21,41 @@ let almacenamiento = document.getElementById(`almacenamiento`);
 let placaGrafica = document.getElementById(`placa`); 
 let ram = document.getElementById(`ram`); 
 let procesador = document.getElementById(`procesador`);
+// variable que cambia a true si el administrador pone que SI esta en oferta
+let enOferta = false;
+// traigo los type radio del formulario
+let siOferta = document.getElementById(`siOferta`)
+let noOferta = document.getElementById(`noOferta`)
+// eventos para si elige Si esta en oferta o No esta en oferta
+siOferta.addEventListener(`click`, ponerEnOferta)
+noOferta.addEventListener(`click`, noPonerEnOferta)
+// Funciones relacionadas a poner o no juego en oferta
+function ponerEnOferta(){
+    enOferta = true;
+    let divPrecioOferta = document.getElementById(`divPrecioOferta`);
+    divPrecioOferta.className = `mb-3`
+    let precioOferta = document.getElementById(`precioOferta`);
+    precioParaJuegoEnOferta = precioOferta;
+    agregarJuegoEnOferta(juego)
+}
+function agregarJuegoEnOferta(juego){
+    if(enOferta == true){
+        listaJuegosOferta.push(juego)
+    }
+}
+function noPonerEnOferta(){
+    enOferta = false;
+}
 
+
+// Aqui empiezan los eventos de botones
 btnCrearJuego.addEventListener(`click`, mostrarFormularioJuego)
 
+// Aqui empiezan las funciones
 function mostrarFormularioJuego(){
     limpiarFormulario()
     modalJuegos.show()
 }
 function limpiarFormulario(){
-    formularioAdminJuego.requestFullscreen()
+    formularioAdminJuego.reset()
 }
