c2370a7b2527943ef58a3ebc2f37c646e4f7fb8e
Leonel2099 <ezequielleonel1@gmail.com>
2023-05-14 14:04:01 -0300

    Se realizo la primera parte de la logica

--- js/app.js
+++ js/app.js
@@ -1,11 +1,21 @@
 // Mostrar y ocultar imagen para dispositivos medianos y grandes en Seccion Recomendados
-let imagenMostrada = document.getElementById('imagenRecomendados')
+let imagenMostrada = document.getElementById("imagenRecomendados");
 
 function mostrarImagen(urlImagen) {
-    imagenMostrada.src = urlImagen;
-  }
+  imagenMostrada.src = urlImagen;
+}
 
-  function imagenPorDefecto() {
-    let imagenMostrada = document.getElementById("imagenRecomendados");
-    imagenMostrada.src = 'img/Gif_logo_negro.gif';
-  }
+function imagenPorDefecto() {
+  let imagenMostrada = document.getElementById("imagenRecomendados");
+  imagenMostrada.src = "img/Gif_logo_negro.gif";
+}
+//Cambiar temas de las paginas
+let botonTemaOscuro = document.querySelector("#btnTemaOscuro");
+let botonTemaClaro = document.querySelector("#btnTemaClaro");
+
+botonTemaOscuro.addEventListener("click", () =>cambiarTema("dark"));
+botonTemaClaro.addEventListener("click", () =>cambiarTema("light"));
+
+function cambiarTema(colorTema){
+  document.querySelector("body").setAttribute("data-bs-theme", colorTema);
+}
