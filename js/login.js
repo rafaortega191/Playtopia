1bbc70d8fe474bb79725c2d15b2ec86af1731a1e
Geronimo <cossiogeronimo601@gmail.com>
2023-05-16 19:23:23 -0300

    Se cambian los var por let

--- js/login.js
+++ js/login.js
@@ -1,10 +1,10 @@
 function validarFormulario() {
-  var usuario = document.getElementById("usuarioInput").value;
-  var contrasenia = document.getElementById("contraseniaInput").value;
+  let usuario = document.getElementById("usuarioInput").value;
+  let contrasenia = document.getElementById("contraseniaInput").value;
   let alertLogin = document.getElementById('alert_login');
 
-  var usuarioValidacion = /^[a-zA-Z0-9]{2,50}$/;
-  var contraseniaValidacion = /^.{6,20}$/;
+  let usuarioValidacion = /^[a-zA-Z0-9]{2,50}$/;
+  let contraseniaValidacion = /^.{6,20}$/;
 
   if (!usuarioValidacion.test(usuario)) {
     alertLogin.classList.remove("d-none");
